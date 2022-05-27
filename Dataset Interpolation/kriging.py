95ee32ee520ddef31a171c112e1463a23f54cc2f
unknown <jteh0009@student.monash.edu>
2022-05-27 19:54:12 +0800

    update kriging.py

--- Dataset Interpolation/kriging.py
+++ Dataset Interpolation/kriging.py
@@ -11,11 +11,51 @@ ot.Log.Show(ot.Log.NONE)
 read data from rssi.csv file and filter 
 it to access point A in floor 1
 """
-
 data = pd.read_csv('rssi.csv')
-flt = data['ap'] == 'A'
+
+
+
+
+while True:
+    #input from user about desired access point
+    choices = "Access Points: A, B, C, D"
+
+    print(choices)
+    val = input('Enter your desired access point: ')
+    val = val.upper()
+    print(val)
+    if val == 'A':
+        break
+    elif val == 'B':
+        break
+    elif val == 'C':
+        break
+    elif val == 'D':
+        break
+    else:
+        print("please enter valid access point")
+        continue
+
+while True:
+    #input from user about desired floor
+    fchoices = "Floor: 1 or 2?"
+
+    print(fchoices)
+    val2 = float(input('Enter your desired floor: '))
+    print(val2)
+
+    if val2 == 1.0:
+        break
+    elif val2 == 2.0:
+        break
+    else:
+        print("please enter valid floor")
+        continue
+
+
+flt = data['ap'] == val
 data2 = data[flt]
-data3 = data2['z'] == 1.0
+data3 = data2['z'] == val2
 data3 = data2[data3]
 
 x = data3['x']
@@ -67,33 +107,75 @@ x_y = ftr(x_y)
 a function to remove some of the datapoints
 to interpolate them again
  """
+
+dlt_points = []
+
 def delete_random_elems(input_list, n):
     to_delete = set(random.sample(range(len(input_list)), n))
+
+    for i in to_delete:
+        dlt_points.append(input_list[i])
+
     return [x for i,x in enumerate(input_list) if not i in to_delete]
 
 x_y = delete_random_elems(x_y, 81)
 
 
-
 new_xy = []
 new_z = []
 
+dlt_xy = []
+dlt_z = []
 """
 a function to take the coordinates into separte 
 list from the values list
 """
 
 
-def adjust(l):
-    for i in range(0, len(x_y) - 1):
-        new_xy.append(x_y[i][0])
-        new_z.append(x_y[i][1])
+def adjust(l, xy, z):
+    for i in range(0, len(l)):
+        xy.append(l[i][0])
+        z.append(l[i][1])
         continue
 
     return new_xy
 
 
-adjust(x_y)
+adjust(x_y, new_xy, new_z)
+adjust(dlt_points, dlt_xy, dlt_z)
+
+
+"""
+def tst_accuracy(pred, vert):
+    match = 0
+    for i in range(0, len(pred)-1):
+        for j in range(0, len(pred) - 1):
+            if round(pred[i][0]) == dlt_z[j] and round(vert[i][0]) == dlt_xy[j][0] and round(vert[i][1]) == dlt_xy[j][1]:
+                match += 1
+                break
+            else:
+                j += 1
+                continue
+    print(match)
+    return match
+"""
+
+
+
+def tst_accuracy(pred, vert):
+    match = 0
+    for i in range(0, len(dlt_xy)-1):
+        for j in range(0, len(pred) - 1):
+            if round(pred[j][0]) == dlt_z[i] and round(vert[j][0]) == dlt_xy[i][0] and round(vert[j][1]) == dlt_xy[i][1]:
+                match += 1
+                break
+            else:
+                j += 1
+                continue
+    print("Number of matches = " + str(match) + " out of " + str(len(dlt_xy)))
+
+    return match
+
 
 
 # function to put each value into list
@@ -105,6 +187,7 @@ new_z = extractDigits(new_z)
 
 new_xy = ot.Sample(new_xy)
 new_z = ot.Sample(new_z)
+
 
 # Extract coordinates.
 x = np.array(new_xy[:, 0])
@@ -169,8 +252,8 @@ def plotKrigingPredictions(krigingMetamodel):
     myInterval = ot.Interval([0., 0.], [35.0, 45.0])
 
     # Define the number of intervals in each direction of the box
-    nx = 9
-    ny = 9
+    nx = 500
+    ny = 400
     myIndices = [nx - 1, ny - 1]
     myMesher = ot.IntervalMesher(myIndices)
     myMeshBox = myMesher.build(myInterval)
@@ -178,6 +261,9 @@ def plotKrigingPredictions(krigingMetamodel):
     # Predict
     vertices = myMeshBox.getVertices()
     predictions = krigingMetamodel(vertices)
+
+    #print(round(vertices[1][0]))
+    tst_accuracy(predictions, vertices)
 
     # write to csv
 
@@ -187,6 +273,7 @@ def plotKrigingPredictions(krigingMetamodel):
     X = np.array(vertices[:, 0]).reshape((ny, nx))
     Y = np.array(vertices[:, 1]).reshape((ny, nx))
     predictions_array = np.array(predictions).reshape((ny, nx))
+
 
     # Plot
     plt.figure()
@@ -201,9 +288,9 @@ inputDimension = 2
 basis = ot.ConstantBasisFactory(inputDimension).build()
 isotropic = ot.IsotropicCovarianceModel(
     ot.SquaredExponential(), inputDimension)
+
 krigingResult, krigingMetamodel = fitKriging(
     new_xy, new_z, isotropic, basis)
-
 plotKrigingPredictions(krigingMetamodel)
 
 """
